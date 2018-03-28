55f00bd65d3f3d122abc7164ad7ad3aea70c60c3
FJQPeter <450742605@qq.com>
2018-03-28 23:16:30 +0800

    mall

---
+++ demo/src/main/java/com/mmall/StringTest.java
@@ -0,0 +1,15 @@
+package com.mmall;
+
+import org.junit.Test;
+
+/**
+ * Created by FangYan on 2018/3/7.
+ */
+public class StringTest {
+    private static String a="";
+    @Test
+    public void test(){
+        System.out.println(null!="");
+    }
+
+}
